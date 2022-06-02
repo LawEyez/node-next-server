8e66aa4db7318ecde4f21c5195e0d957c1c06401
Bioga Allen <allenojuka10@gmail.com>
2022-06-02 21:43:45 +0300

    Loaded data.json using require.

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 import express from 'express'
+import { createRequire } from 'module' // Bring in ability to create 'require' method. ('import data from data.json' doesn't work.)
+
+// Construct the require method.
+const require = createRequire(import.meta.url)
+
+// Read json data.
+const data = require('./data.json')
 
 const app = express()
 const port = 5000
 
+/**
+ * Get users.
+ * @method: GET
+ */
+app.get('/users', (req, res) => {
+  const users = data.users
+  res.status(200).send(users)
+})
+
+// Start server.
 app.listen(port, () => console.log('Server is live on port ' + port))
